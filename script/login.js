a8f16baedb6ae5c13bba50f98a0ab18f95cecd08
Mahabub Islam <marufhasan7702@gmail.com>
2026-02-20 01:30:22 +0600

    Login Verification Using JavaScript

---
+++ script/login.js
@@ -0,0 +1,16 @@
+document.getElementById('login-btn')
+.addEventListener('click', function() {
+    const loginInput = document.getElementById('login-input');
+    const mobileNumber = loginInput.value;
+
+    const loginPin = document.getElementById('login-pin');
+    const pin = loginPin.value;
+
+    if(mobileNumber === '01234567890' && pin === '1234') {
+        alert('Login successful!');
+
+        window.location.assign("/home.html");
+    } else {
+        alert('Invalid mobile number or pin. Please try again.');
+    }
+});
